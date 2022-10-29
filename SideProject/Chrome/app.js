f5ca2f1a05b72e593802fc7437cc0850efbeb9ea
임우진 <uzleem.github.com>
2022-10-29 21:45:29 +0900

    login

--- SideProject/Chrome/app.js
+++ SideProject/Chrome/app.js
@@ -1,1 +1,22 @@
-alert("hi");
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+const HIDDEN_CLASSNAME = "hidden";
+
+function onLoginSubmit(event) {
+    event.preventDefault();
+
+    // js로 class설정
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+ 
+    const userName = loginInput.value;
+ 
+    // 벡틱(`) 방식으로 사용 시 + 없이 구문 추가
+    // greeting.innerHTML = "Hello " + userName;
+    greeting.innerHTML = `Hello! ${userName}`;
+
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+
+}
+
+loginForm.addEventListener("submit", onLoginSubmit);
