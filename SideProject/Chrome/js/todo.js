6f4e63733533ad41ff0cac2797445647ffdf69d8
임우진 <uzleem.github.com>
2022-11-27 19:32:00 +0900

    chrome last

--- SideProject/Chrome/js/todo.js
+++ SideProject/Chrome/js/todo.js
@@ -1,17 +1,18 @@
 const todoForm = document.getElementById("todo-form");
 const todoInput = todoForm.querySelector("input");
 const todoList = document.getElementById("todo-list");
-const toDos = [];
+let toDos = [];
 const TODOS_KEY = "todos"
+
 
 function saveTodos() {
     /** Storage에 문자열 형식으로 값을 저장하기위해 JSON.stringify를 사용 
      *  1. : push(a), push(b), push(c)
      *  2. : '["a", "b", "c"]'
     */
-
     localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));    
 }
+
 
 function deleteTodo(event) {
     /** 
@@ -19,50 +20,55 @@ function deleteTodo(event) {
      *  2. parentElement를 통해 해당 버튼의 부모의 접근
      *  3. 해당항목 삭제 
      * */
-    
     const li = event.target.parentElement;
     li.remove();
+
+    /** todo filter */
+    toDos = toDos.filter((todo) => todo.id !== parseInt(li.id));
+
+    saveTodos();
 }
+
 
 function paintTodo(newTodo) {
     /* html의 적용시킬 대상 생성 */
     const li = document.createElement("li");
-    
+    li.id = newTodo.id;
+
     const span = document.createElement("span");
-    span.innerText = newTodo;
+    span.innerText = newTodo.text;
     
     const buttonX = document.createElement("button");
     buttonX.innerText = "X";
 
     buttonX.addEventListener("click", deleteTodo);
-
     
     /** li안의 span 적용 */
     li.appendChild(span);
     li.appendChild(buttonX);    
-
-    /** todo-list내부의 li값 적용 */
-    todoList.appendChild(li);
    
+    todoList.appendChild(li);
 }
 
+
+
 function handleToDoSubmit(event) {
+    
     event.preventDefault();
+    
+    const newTodoObj = {
+        text : todoInput.value
+      , id : Date.now()
+    }
 
     const newTodo = todoInput.value;
     todoInput.value = "";
-
-
-    toDos.push(newTodo);
-    paintTodo(newTodo);
+    
+    toDos.push(newTodoObj);
+    paintTodo(newTodoObj);
 
     saveTodos();
 } 
-
-function sayHello(item) {
-    console.log("hi", item);
-
-}
 
 
 /** JSON.stringify을 통해 담은 문자열을 실제 사용하기 위해 JSON.parse 사용
@@ -79,12 +85,13 @@ const saveTodo = localStorage.getItem(TODOS_KEY);
  */
 if(saveTodo !== null) {
     const parseTodo = JSON.parse(saveTodo);
-    console.log(parseTodo);
-    parseTodo.forEach(sayHello);
-    // parseTodo.forEach(element => {console("hello", element)}); //간단한 foreach
-
     
+    toDos = parseTodo
+
+    parseTodo.forEach(paintTodo);
 }
+
+
 
 
 /** todoForm submit 이벤트 처리*/
