283e3996fa5d8180488e3c481cfd39134401b030
임우진 <uzleem.github.com>
2023-02-26 22:02:09 +0900

    [router] react router 공부
    1. react reouter 공부
    2. notion 공부
    3. notion 정리
    4. git 작성규칙

--- SideProject/ReactJS/movieweb/src/App.js
+++ SideProject/ReactJS/movieweb/src/App.js
@@ -1,46 +1,16 @@
-import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Home from "./routes/Home";
+import Detail from "./routes/Detail";
 
 function App() {
-  const [loding, setLoding] = useState(true);
-  const [movie, setMovie] = useState([]);
-
-  const movieJson = async () => {
-    const json = await (
-      await fetch(
-        `https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`
-      )
-    ).json();
-
-    setMovie(json.data.movies);
-    setLoding(false);
-  };
-  console.log(movie);
-
-  useEffect(() => {
-    movieJson();
-  }, []);
-
   return (
-    <div>
-      {loding ? (
-        <h1>Loding...</h1>
-      ) : (
-        <div>
-          {movie.map((item) => (
-            <div key={item.id}>
-              <h2>{item.title}</h2>
-              <img src={item.medium_cover_image}></img>
-              <p>{item.summary}</p>
-              <ul>
-                {item.genres.map((item2) => (
-                  <li key={item2}>{item2}</li>
-                ))}
-              </ul>
-            </div>
-          ))}
-        </div>
-      )}
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/movie" element={<Detail />}></Route>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
+    </Router>
   );
 }
 
---
+++ SideProject/ReactJS/movieweb/src/routes/Detail.js
@@ -0,0 +1,5 @@
+function Detail() {
+  return <h1>render성공</h1>;
+}
+
+export default Detail;
